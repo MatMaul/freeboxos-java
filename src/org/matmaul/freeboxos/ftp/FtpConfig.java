c72bf830159586d0cdda3a97b4af788ce666b09d
lolodomo <lg.hc@free.fr>
2016-02-28 19:00:34 +0100

    Fix FTP config compatibility with firmware 3.3

--- src/org/matmaul/freeboxos/ftp/FtpConfig.java
+++ src/org/matmaul/freeboxos/ftp/FtpConfig.java
@@ -6,9 +6,14 @@ import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
 public class FtpConfig {
 	protected Boolean enabled;
-	protected boolean allow_anonymous;
-	protected boolean allow_anonymous_write;
+	protected Boolean allow_anonymous;
+	protected Boolean allow_anonymous_write;
 	protected String password;
+	protected Boolean weak_password;
+	protected Boolean allow_remote_access;
+	protected String remote_domain;
+	protected int port_ctrl;
+	protected int port_data;
 	
 	public Boolean getEnabled() {
 		return enabled;
@@ -22,6 +27,21 @@ public class FtpConfig {
 	public String getPassword() {
 		return password;
 	}
+	public Boolean getWeakPassword() {
+		return weak_password;
+	}
+	public Boolean getAllowRemoteAccess() {
+		return allow_remote_access;
+	}
+	public String getRemoteDomain() {
+		return remote_domain;
+	}
+	public int getPortCtrl() {
+		return port_ctrl;
+	}
+	public int getPortData() {
+		return port_data;
+	}
 	public void setEnabled(Boolean enabled) {
 		this.enabled = enabled;
 	}
@@ -33,6 +53,21 @@ public class FtpConfig {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public void setWeakPassword(Boolean weak_password) {
+		this.weak_password = weak_password;
+	}
+	public void setAllowRemoteAccess(Boolean allow_remote_access) {
+		this.allow_remote_access = allow_remote_access;
+	}
+	public void setRemoteDomain(String remote_domain) {
+		this.remote_domain = remote_domain;
+	}
+	public void setPortCtrl(int port_ctrl) {
+		this.port_ctrl = port_ctrl;
+	}
+	public void setPortData(int port_data) {
+		this.port_data = port_data;
 	}
 
 }
